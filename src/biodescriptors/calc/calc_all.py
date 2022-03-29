67204e01f945920f71ab04214a666788b37b3a80
GlukhovIgor <gluhov2000@gmail.com>
2022-03-29 17:40:29 +0300

    allow for use of .ent format

--- src/biodescriptors/calc/calc_all.py
+++ src/biodescriptors/calc/calc_all.py
@@ -89,7 +89,7 @@ class DescCalculator:
 
         """
         filenames = os.listdir(filedir)
-        full_filenames = [os.path.join(filedir, filename) for filename in filenames if '.pdb' in filename]
+        full_filenames = [os.path.join(filedir, filename) for filename in filenames if any([elem in filename for elem in ['pdb', 'ent']])]
         number_files = len(full_filenames)
 
         results_dfs = []
