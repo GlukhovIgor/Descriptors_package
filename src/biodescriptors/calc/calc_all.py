4582c81a174ba42d6c9fdf770f51798c7457b8c0
GlukhovIgor <gluhov2000@gmail.com>
2022-08-25 17:12:12 +0300

    remove test function

--- src/biodescriptors/calc/calc_all.py
+++ src/biodescriptors/calc/calc_all.py
@@ -112,46 +112,6 @@ class DescCalculator:
             final_df.to_csv(output_full_path, index=False)
         return final_df
 
-    def test_func():
-        """
-        Clip (limit) the values in an array.
-
-        Given an interval, values outside the interval are clipped to
-        the interval edges.  For example, if an interval of ``[0, 1]``
-        is specified, values smaller than 0 become 0, and values larger
-        than 1 become 1.
-
-        Equivalent to but faster than ``np.minimum(a_max, np.maximum(a, a_min))``.
-
-        No check is performed to ensure ``a_min < a_max``.
-
-        Parameters
-        ----------
-        a : array_like
-            Array containing elements to clip.
-        a_min, a_max : array_like or None
-            Minimum and maximum value. If ``None``, clipping is not performed on
-            the corresponding edge. Only one of `a_min` and `a_max` may be
-            ``None``. Both are broadcast against `a`.
-        out : ndarray, optional
-            The results will be placed in this array. It may be the input
-            array for in-place clipping.  `out` must be of the right shape
-            to hold the output.  Its type is preserved.
-        **kwargs
-            For other keyword-only arguments, see the
-            :ref:`ufunc docs <ufuncs.kwargs>`.
-
-            .. versionadded:: 1.17.0
-
-        Returns
-        -------
-        clipped_array : ndarray
-            An array with the elements of `a`, but where values
-            < `a_min` are replaced with `a_min`, and those > `a_max`
-            with `a_max`.
-        """
-        pass
-
 # ____________NAME_TO_DESCRIPTOR_MAPPING________________ #
 
 
