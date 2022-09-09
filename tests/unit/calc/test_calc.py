a18e1ac2e952f5e9e4c9a2efb6ae38c492b65a08
GlukhovIgor <gluhov2000@gmail.com>
2022-09-09 18:04:40 +0300

    linter fixes for tests v2

--- tests/unit/calc/test_calc.py
+++ tests/unit/calc/test_calc.py
@@ -3,46 +3,46 @@ from pytest import approx
 
 
 test_structure = 'tests/data/1DB1.pdb'
-href = [[127,142], [149,152], [226,246]]
+href = [[127, 142], [149, 152], [226, 246]]
 
 
-def test_calc_len_of_hel():    
+def test_calc_len_of_hel():
     ans = calc.calc_len_of_hel(test_structure, href)
     correct_ans = [approx(x, rel=0.1) for x in [22.6, 5.6, 30.5]]
     for i, elem in enumerate(correct_ans):
         assert_out_str = (
             "Value {0} at index [{1}] doesn't fall into expected range: {2}"
             .format(
-                round(ans[i], 2), 
-                i, 
+                round(ans[i], 2),
+                i,
                 elem)
         )
         assert ans[i] == elem, assert_out_str
 
 
-def test_calc_COM_Calpha_angles():    
+def test_calc_COM_Calpha_angles():
     ans = calc.calc_COM_Calpha_angles(test_structure, href)
     correct_ans = [approx(x, rel=0.1) for x in [61.0, 14.2, 120.5]]
     for i, elem in enumerate(correct_ans):
         assert_out_str = (
             "Value {0} at index [{1}] doesn't fall into expected range: {2}"
             .format(
-                round(ans[i], 2), 
-                i, 
+                round(ans[i], 2),
+                i,
                 elem)
         )
         assert ans[i] == elem, assert_out_str
 
 
-def test_calc_COM_protein():    
+def test_calc_COM_protein():
     ans = calc.calc_COM_protein(test_structure)
     correct_ans = [approx(x, rel=0.1) for x in [13.360, 20.776, 42.003]]
     for i, elem in enumerate(correct_ans):
         assert_out_str = (
             "Value {0} at index [{1}] doesn't fall into expected range: {2}"
             .format(
-                round(ans[i], 2), 
-                i, 
+                round(ans[i], 2),
+                i,
                 elem)
         )
         assert ans[i] == elem, assert_out_str
@@ -61,7 +61,7 @@ def test_calc_COM_protein():
 #     assert ans == correct_ans
 
 
-def test_calc_COM_helix():    
+def test_calc_COM_helix():
     ans = calc.calc_COM_helix(test_structure, href)
     correct_ans = [[[approx(x, rel=0.1) for x in [18.888, 6.445, 50.056]]],
                    [[approx(x, rel=0.1) for x in [10.596, 10.219, 26.464]]],
@@ -71,22 +71,22 @@ def test_calc_COM_helix():
             assert_out_str = (
                 "Value {0} at index [{1}][0][{2}] doesn't fall into expected range: {3}"
                 .format(round(ans[i][0][j], 2), 
-                        i, 
-                        j, 
+                        i,
+                        j,
                         elem2)
             )
             assert ans[i][0][j] == elem2, assert_out_str
 
 
-def test_calc_prot_hel_dist():    
+def test_calc_prot_hel_dist():
     ans = calc.calc_prot_hel_dist(test_structure, href)
     correct_ans = [approx(x, rel=0.1) for x in [17.6, 18.4, 9.9]]
     for i, elem in enumerate(correct_ans):
         assert_out_str = (
             "Value {0} at index [{1}] doesn't fall into expected range: {2}"
             .format(
-                round(ans[i], 2), 
-                i, 
+                round(ans[i], 2),
+                i,
                 elem)
         )
         assert ans[i] == elem, assert_out_str
